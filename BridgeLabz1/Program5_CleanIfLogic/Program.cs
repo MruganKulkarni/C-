dc8b0f3e5416cd651b55355c5f499f3452a53868
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 15:26:47 +0530

    If program done

---
+++ BridgeLabz1/Program5_CleanIfLogic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter marks: ");
+        int marks = int.Parse(Console.ReadLine());
+
+        if (marks >= 0 && marks <= 100 && marks >= 40)
+        {
+            Console.WriteLine("Pass");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+}
