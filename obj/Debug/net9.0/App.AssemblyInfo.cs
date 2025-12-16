e25fb3e762fa9c33c21ed42d8b6f906d0b74b9d6
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 11:41:19 +0530

    Initial commit

--- obj/Debug/net9.0/App.AssemblyInfo.cs
+++ obj/Debug/net9.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939a454eb93f56d550e982d637116ce71d7dbd0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
