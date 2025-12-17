b4a15c7c2dfe8fe962d60f55816b592d25565d35
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-17 11:28:28 +0530

    5 Level1 Questions completed

--- BridgeLabz2/Level1Problems/Program4_NaturalNumberSum/Program.cs
+++ BridgeLabz2/Level1Problems/Program4_NaturalNumberSum/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number: ");
+        int num = int.Parse(Console.ReadLine());
+        if ( num >= 0)
+        {
+            int sum = (num * (num + 1)) / 2;
+            Console.WriteLine($"The sum of {num} natural numbers is {sum}");
+        }
+        else
+        {
+            Console.WriteLine($"The number {num} is not a natural number");
+        }
+
+    }
+}
